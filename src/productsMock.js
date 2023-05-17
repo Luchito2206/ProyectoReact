c855c73bc62ef3ed0a8bcdc35f4098face52004b
Luciano Palacios Longo <lucianopalacioslongo@gmail.com>
2023-05-17 13:00:30 -0300

    arreglando para entrega final

--- src/productsMock.js
+++ src/productsMock.js
@@ -1,7 +1,7 @@
 
 export const products = [
     {
-        id: 1,
+      
         title: "Nike 360",
         price: 15000,
         stock: 2,
@@ -10,7 +10,7 @@ export const products = [
         img: "https://res.cloudinary.com/dnqfh2chg/image/upload/v1669326932/free-metcon-4-zapatillas-de-entrenamiento-TQMCZg_fvia3m.png"
     },
     {
-        id: 2,
+        
         title: "wilson advance",
         price: 14500,
         stock: 15,
@@ -19,7 +19,7 @@ export const products = [
         img: "https://res.cloudinary.com/dnqfh2chg/image/upload/v1669430010/foc364fsakkzmausrovn.jpg"
     },
     {
-        id: 3,
+        
         title: "Puma",
         price: 19000,
         stock: 3,
@@ -28,7 +28,7 @@ export const products = [
         img: "https://res.cloudinary.com/dnqfh2chg/image/upload/v1669430165/images_lujpmr.jpg"
     },
     {
-        id: 4,
+       
         title: "Jaguar",
         price: 17800,
         stock: 5,
@@ -37,39 +37,13 @@ export const products = [
         img: "https://res.cloudinary.com/dnqfh2chg/image/upload/v1669430220/rtokwmt3j03bblbfhwhb.jpg"
     },
     {
-        id: 5,
-        title: "Nike",
-        price: 25000,
-        stock: 3,
-        description: "Air Max SYSTM recupera todo lo que te encanta de tus vibraciones favoritas de los 80",
-        category: "urbanas",
-        img: "https://res.cloudinary.com/diwpmquqv/image/upload/v1682344356/zapatillas-nike-air-max-systm-gris-510010dm9537002-1_mhawib.jpg"
-    },
-    {
-        id: 6,
-        title: "ADIDAS",
-        price: 27500,
+       
+        title: "Jaguar",
+        price: 17800,
         stock: 5,
-        description: "Inyectale la comodidad y el estilo deportivo del calzado de running a tu look diario con estas zapatillas ADIDAS",
+        description: "Zapatillas de ultima generacion para el deporte",
         category: "urbanas",
-        img: "https://res.cloudinary.com/diwpmquqv/image/upload/v1682344378/zapatillas-adidas-racer-tr21-gris-21308057-100010gx4238001-1_dvdabq.jpg"
-    },
-    {
-        id: 7,
-        title: "ADIDAS",
-        price: 32000,
-        stock: 3,
-        description: "Corre para alcanzar tus metas",
-        category: "deportivas",
-        img: "https://res.cloudinary.com/diwpmquqv/image/upload/v1682344389/100010ee9843001-1_ehztbm.jpg"
-    },
-    {
-        id: 8,
-        title: "Asics",
-        price: 15000,
-        stock: 5,
-        description: "La zapatilla NOOSA TRI™ 14 remezcla la inspiración clásica con un ajuste tipo calcetín",
-        category: "deportivas",
-        img: "https://res.cloudinary.com/diwpmquqv/image/upload/v1682344396/zapatillas-running-asics-noosa-tri-14-standard-naranja-13001011b368800-1_ovruiv.jpg"
+        img: "https://res.cloudinary.com/dnqfh2chg/image/upload/v1669430220/rtokwmt3j03bblbfhwhb.jpg"
     }
+
 ]
